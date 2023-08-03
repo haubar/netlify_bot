fb32a23d419f631210cf7ebf7a2260648ae50516
haubar <haubarclack@gmail.com>
2023-08-03 21:18:44 +0800

    fix stock

--- bot/stock/stock.js
+++ bot/stock/stock.js
@@ -1,24 +1,58 @@
 const line = require('@line/bot-sdk')
 const fetch = require("node-fetch")
 
+
+const handler = async (event) => {
+
 const clientConfig = {
 	channelAccessToken: process.env.CHANNEL_TOKEN,
 	channelSecret: process.env.CHANNEL_SECRET
   } 
 
-const client = new line.Client(clientConfig);
+const client = new line.Client(clientConfig)
 
-const handler = async (event, context) => {
+const signature = event.headers['x-line-signature'];
+const body = event.body;
 
-	//驗證格式
-	const { replyToken } = event;
+
+//line邏輯處理
+const botEvent = async (event, context) => {
+	if (event.type !== 'message' || event.message.type !== 'text') {
+      		return Promise.resolve(null)
+    	}
+	
+	const { text } = event.message
+	const { replyToken } = event
 	const response = {
 		type: 'text',
-		text: `我還沒空支援這個格式...test`,
+		text: `我還沒空支援這個格式...${text}`,
 	}
 	await client.replyMessage(replyToken, response)
+}
+
+try {
+    // 用 CHANNEL_SECRET 來驗證 Line Bot 身分
+    if (!line.validateSignature(body, clientConfig.channelSecret, signature)) {
+      throw new line.exceptions.SignatureValidationFailed("signature validation failed", signature)
+    }
+
+    // 將 JSON 轉為 JavaScript 物件
+    const objBody = JSON.parse(body);
+    // 將觸發事件交給 Line Bot 做處理
+    await Promise.all(objBody.events.map(botEvent))
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello from Netlify" }),
+    }
+  } catch (error) {
+    console.log(error)
+    return { statusCode: 500, body: error.toString() }
+  }
 
 }
+
+
 
 //輸出
 module.exports = { handler }
