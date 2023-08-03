55e7f6411148ec989b660668fd2a3aabf37fc21e
haubar <haubarclack@gmail.com>
2023-08-03 20:14:43 +0800

    update

--- bot/stock/stock.js
+++ bot/stock/stock.js
@@ -1,4 +1,3 @@
-require("dotenv").config()
 const line = require('@line/bot-sdk')
 const fetch = require("node-fetch")
 
@@ -9,21 +8,6 @@ const clientConfig = {
 
 const client = new line.Client(clientConfig);
 
-
-// const API_ROOT = 'https://api.unsplash.com';  
-/*
-const handler = async (event) => {
-	try {
-	  const subject = event.queryStringParameters.name || 'World, this is test function'
-	  return {
-		statusCode: 200,
-		body: JSON.stringify({ message: `Hello ${subject}` }),
-	  }
-	} catch (error) {
-	  return { statusCode: 500, body: error.toString() }
-	}
-  }
-*/
 const handler = async (event, context) => {
 
 	//驗證格式
@@ -34,40 +18,6 @@ const handler = async (event, context) => {
 	}
 	await client.replyMessage(replyToken, response)
 
-	// 如果是訊息而且訊息是文字的話
-	// 需再拆分
-	/*
-	if (event.type == 'message' || event.message.type == 'text') {
-		
-		const { replyToken } = event;
-    	const { text } = event.message;
-		//回應方式及內容
-		const response = {
-			type: 'text',
-			text: `我現在只會重覆你講的話：${text}～`,
-		}
-	} 
-	//line sdk 回應訊息
-	await client.replyMessage(replyToken, response)
-	*/
-	//驗證
-	/*
-	try {
-		const response = await fetch(API_ENDPOINT);
-		const data = await response.json();
-		return { 
-			statusCode: 200, 
-			body: JSON.stringify({ message: "debug message" }) }
-			// 回應內容
-			// body: JSON.stringify({ data })
-	  } catch (error) {
-		console.log(error)
-		return {
-		  statusCode: 500,
-		  body: JSON.stringify({ error: 'Failed fetching data' }),
-		}
-	  }
-	*/
 }
 
 //輸出
