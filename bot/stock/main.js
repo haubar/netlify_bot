e6c954ca1ff44f20aaf472958f48d2199e5db061
haubar <haubarclack@gmail.com>
2023-09-18 16:50:37 +0800

    修改主程度命名

---
+++ bot/stock/main.js
@@ -0,0 +1,28 @@
+const message = require('../lib/message')
+const fstock = require('../lib/stock')
+
+const handler = async (event) => {
+
+  const body = event.body
+
+  try {
+      // JSON格式轉換物件
+      const objBody = JSON.parse(body);
+      // 觸發事件轉linebot處理
+      // await Promise.all(objBody.events.map(botEvent))
+      await Promise.all(objBody.events.map(message))
+
+      //無觸發的預設返回訊息
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "聽嘸哩共蝦咪..." }),
+      }
+  } catch (error) {
+        console.log(error)
+        return { statusCode: 500, body: error.toString() }
+  }
+
+}
+
+//主程式輸出
+module.exports = { handler }
