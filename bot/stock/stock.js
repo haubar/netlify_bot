c586e138419e2b794f93df6a857d3e5e21cf8fa9
haubar <haubarclack@gmail.com>
2023-09-18 17:37:57 +0800

    update file

--- bot/stock/stock.js
+++ bot/stock/stock.js
@@ -1,4 +1,4 @@
-const message = require('../lib/message')
+// const message = require('../lib/message')
 // const fstock = require('../lib/stock')
 
 const handler = async (event) => {
@@ -9,7 +9,7 @@ const handler = async (event) => {
       // JSON格式轉換物件
       const objBody = JSON.parse(body);
       // 觸發事件轉linebot處理
-      // await Promise.all(objBody.events.map(message))
+      await Promise.all(objBody.events.map(botEvent))
 
       //無觸發的預設返回訊息
       return {
@@ -22,6 +22,78 @@ const handler = async (event) => {
   }
 
 }
+
+
+const botEvent = async (event, context) => {
+    
+  //只接收訊息格式
+  if (event.type !== 'message') {
+      return Promise.resolve(null)
+  }
+
+  switch (event.message.type) {
+    case 'text':
+      const { text } = event.message
+      const res = {
+          type: 'text',
+          text: `我收到的訊息是.... ${text}`,
+      }
+      break
+    case 'sticker':
+      const res = {
+        "type": "sticker",
+        "packageId": "1",
+        "stickerId": "1"
+      }
+      break
+    case 'image':
+      const res = {
+        "type": "image",
+        "originalContentUrl": "圖片網址",
+        "previewImageUrl": "縮圖網址"
+      }
+      break
+    case 'video':
+      
+      break
+    case 'audio':
+      const res = {
+        "type": "audio",
+        "originalContentUrl": "聲音檔網址",
+        "duration": 60000
+      }
+      break
+    case 'location':
+      const res = {
+        "type": "location",
+        "title": "第一行文字",
+        "address": "第二行文字",
+        "latitude": 35.65910807942215,
+        "longitude": 139.70372892916203
+      }
+      break
+    case 'imagemap':
+      const res = {
+        type: 'text',
+        text: `還沒實作這個格式`,
+      }
+      break
+    case 'template':
+      const res = {
+        type: 'text',
+        text: `還沒實作這個格式`,
+      }
+      break
+    default:
+      break
+  }
+
+  const { replyToken } = event
+  const response = res
+  
+  await client.replyMessage(replyToken, response)
+}
+
 
 //主程式輸出
 module.exports = { handler }
