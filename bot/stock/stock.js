abd36e36fc1a68123302d80ad7b51e899e1932cb
haubar <haubarclack@gmail.com>
2023-09-18 17:59:35 +0800

    update message

--- bot/stock/stock.js
+++ bot/stock/stock.js
@@ -38,70 +38,67 @@ const botEvent = async (event, context) => {
   if (event.type !== 'message') {
       return Promise.resolve(null)
   }
-
-  // switch (event.message.type) {
-  //   case 'text':
-  //     const { text } = event.message
-  //     const res = {
-  //         type: 'text',
-  //         text: `我收到的訊息是.... ${text}`,
-  //     }
-  //     break
-  //   case 'sticker':
-  //     const res = {
-  //       "type": "sticker",
-  //       "packageId": "1",
-  //       "stickerId": "1"
-  //     }
-  //     break
-  //   case 'image':
-  //     const res = {
-  //       "type": "image",
-  //       "originalContentUrl": "圖片網址",
-  //       "previewImageUrl": "縮圖網址"
-  //     }
-  //     break
-  //   case 'video':
-      
-  //     break
-  //   case 'audio':
-  //     const res = {
-  //       "type": "audio",
-  //       "originalContentUrl": "聲音檔網址",
-  //       "duration": 60000
-  //     }
-  //     break
-  //   case 'location':
-  //     const res = {
-  //       "type": "location",
-  //       "title": "第一行文字",
-  //       "address": "第二行文字",
-  //       "latitude": 35.65910807942215,
-  //       "longitude": 139.70372892916203
-  //     }
-  //     break
-  //   case 'imagemap':
-  //     const res = {
-  //       type: 'text',
-  //       text: `還沒實作這個格式`,
-  //     }
-  //     break
-  //   case 'template':
-  //     const res = {
-  //       type: 'text',
-  //       text: `還沒實作這個格式`,
-  //     }
-  //     break
-  //   default:
-  //     break
-  // }
-
-  const { text } = event.message
-  const { replyToken } = event
-  const response = {
+  const type = event.message.type
+  switch (type) {
+    case 'text':
+      const { text } = event.message
+      const res = {
           type: 'text',
           text: `我收到的訊息是.... ${text}`,
       }
+      break
+    case 'sticker':
+      const res = {
+        "type": "sticker",
+        "packageId": "1",
+        "stickerId": "1"
+      }
+      break
+    case 'image':
+      const res = {
+        "type": "image",
+        "originalContentUrl": "圖片網址",
+        "previewImageUrl": "縮圖網址"
+      }
+      break
+    case 'video':
+      
+      break
+    case 'audio':
+      const res = {
+        "type": "audio",
+        "originalContentUrl": "聲音檔網址",
+        "duration": 60000
+      }
+      break
+    case 'location':
+      const res = {
+        "type": "location",
+        "title": "第一行文字",
+        "address": "第二行文字",
+        "latitude": 35.65910807942215,
+        "longitude": 139.70372892916203
+      }
+      break
+    case 'imagemap':
+      const res = {
+        type: 'text',
+        text: `還沒實作這個格式`,
+      }
+      break
+    case 'template':
+      const res = {
+        type: 'text',
+        text: `還沒實作這個格式`,
+      }
+      break
+    default:
+      break
+  }
+
+  const { replyToken } = event
+  const response = res
+
   await client.replyMessage(replyToken, response)
 }
 
