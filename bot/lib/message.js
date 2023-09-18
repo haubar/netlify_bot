ecf6e0a2a6e589fccb3055f1bcd76114f728fa16
haubar <haubarclack@gmail.com>
2023-09-18 17:19:05 +0800

    加入格式訊息判斷

--- bot/lib/message.js
+++ bot/lib/message.js
@@ -1,25 +1,85 @@
-const airtable = require('airtable')
-const stockdb = new airtable({
-  apiKey: process.env.airtableKey
-}).base('app2oVW62FODpXmq0')
+const line = require('@line/bot-sdk')
 
-const getstock = async (string) => {
-  try {
-    let filter = 'FIND("' +string+ '", {name}) > 0'
-    const records = await base('stock_list').select({
-      maxRecords: 1,
-      view: 'Grid view',
-      filterByFormula: filter
-    }).all()
+// line channel設定
+const clientConfig = {
+    channelAccessToken: process.env.CHANNEL_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+}
+const client = new line.Client(clientConfig)
+
+//line動作、邏輯判斷
+const botEvent = async (event, context) => {
     
-    return id = records[0].get('no') || 0
-    
-  } catch (error) {
-    throw error
+  //只接收訊息格式
+  if (event.type !== 'message') {
+      return Promise.resolve(null)
   }
+
+  switch (event.message.type) {
+    case 'text':
+      const { text } = event.message
+      const res = {
+          type: 'text',
+          text: `我收到的訊息是.... ${text}`,
+      }
+      break
+    case 'sticker':
+      const res = {
+        "type": "sticker",
+        "packageId": "1",
+        "stickerId": "1"
+      }
+      break
+    case 'image':
+      const res = {
+        "type": "image",
+        "originalContentUrl": "圖片網址",
+        "previewImageUrl": "縮圖網址"
+      }
+      break
+    case 'video':
+      
+      break
+    case 'audio':
+      const res = {
+        "type": "audio",
+        "originalContentUrl": "聲音檔網址",
+        "duration": 60000
+      }
+      break
+    case 'location':
+      const res = {
+        "type": "location",
+        "title": "第一行文字",
+        "address": "第二行文字",
+        "latitude": 35.65910807942215,
+        "longitude": 139.70372892916203
+      }
+      break
+    case 'imagemap':
+      const res = {
+        type: 'text',
+        text: `還沒實作這個格式`,
+      }
+      break
+    case 'template':
+      const res = {
+        type: 'text',
+        text: `還沒實作這個格式`,
+      }
+      break
+    default:
+      break
+  }
+
+  const { replyToken } = event
+  const response = res
+  
+  await client.replyMessage(replyToken, response)
 }
 
-const handler = async (event) => {
+
+const getKeyword = async (event) => {
   try {
     //解析關鍵字
     const keyword = event.keyword.trim() || ""
@@ -33,4 +93,5 @@ const handler = async (event) => {
   }
 }
 
-module.exports = { handler }
+
+module.exports = {botEvent}, {getKeyword}
