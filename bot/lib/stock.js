572235b9aefa94e9d20e9c237076466994dc5814
haubar <haubarclack@gmail.com>
2023-09-18 17:19:36 +0800

    主程式修改

---
+++ bot/lib/stock.js
@@ -0,0 +1,25 @@
+const airtable = require('airtable')
+const stockdb = new airtable({
+    apiKey: process.env.airtableKey
+  }).base('app2oVW62FODpXmq0')
+
+
+const getstock = async (string) => {
+  try {
+    let filter = 'FIND("' +string+ '", {name}) > 0'
+    const records = await base('stock_list').select({
+      maxRecords: 1,
+      view: 'Grid view',
+      filterByFormula: filter
+    }).all()
+    
+    return id = records[0].get('no') || 0
+    
+  } catch (error) {
+      throw error
+  }
+}
+
+
+
+module.exports = { getStock }
